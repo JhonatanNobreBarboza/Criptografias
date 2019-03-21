e9be166113630389e0ee4cb28fb0672ade5b5020
Jhonatan Nobre <jh_nbs@outlook.com>
2019-03-20 22:57:40 -0400

    implementação função functionsCifraVigenere
    criação das funções Criptografar e descriptografar

---
+++ js/functionsCifraVigenare.js
@@ -0,0 +1,24 @@
+let alfabeto = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+
+// Criptografar mensagem
+function criptografar(letra, valorLetra){
+	for(let i = 0; i < letra.length; i++){
+		for(let j = 0; j < alfabeto.length; j++){
+			if(alfabeto[j] == letra[i]){
+				return alfabeto[(j + valorLetra)];
+			}
+		}
+	}
+}
+
+// Descriptografar mensagem
+function descriptografar(letra, valorLetra){
+	for(let i = 0; i < letra.length; i++){
+		for(let j = 0; j < alfabeto.length; j += 1){
+			if(letra[i] == alfabeto[j]){
+				return alfabeto[((j - valorLetra)+25)];
+			}
+		}
+	}
+}
